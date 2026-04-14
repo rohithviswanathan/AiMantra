bcb176ff17d059a5f2054c013607f8aa1b95b521
rohithviswanathan <rohithviswanathan3009@gmail.com>
2026-04-14 17:56:21 +0530

    created exports page

---
+++ aimantra/src/components/exports/Exports.js
@@ -0,0 +1,256 @@
+import WebIcon from "@mui/icons-material/LaptopMac";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import AndroidIcon from "@mui/icons-material/Android";
+import AppleIcon from "@mui/icons-material/Apple";
+import CampaignIcon from "@mui/icons-material/Campaign";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import BrushIcon from "@mui/icons-material/Brush";
+import SearchIcon from "@mui/icons-material/Search";
+import CodeIcon from "@mui/icons-material/Code";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import GroupsIcon from "@mui/icons-material/Groups";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PersonIcon from "@mui/icons-material/Person";
+import WorkIcon from "@mui/icons-material/Work";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import PaymentIcon from "@mui/icons-material/Payment";
+import BarChartIcon from "@mui/icons-material/BarChart";
+
+export const services = [
+    {
+      icon: <WebIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "Web Development",
+      text: "Your vision, our expertise - the perfect match for a standout online presence!",
+      path: "/services/web-development"
+    },
+    {
+      icon: <DesignServicesIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "Web Designing",
+      text: "Your journey to a visually appealing and user-friendly website begins here!",
+      path: "/services/web-designing"
+    },
+    {
+      icon: <AndroidIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "Android Development",
+      text: "Where creativity meets code for unparalleled mobile experiences. Your app journey starts here!",
+      path: "/services/mobileapp-development"
+    },
+    {
+      icon: <AppleIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "iOS Development",
+      text: "Your journey to the top of the App Store starts here - where expertise meets excellence in iOS development!",
+      path: "/services/mobileapp-development"
+    },
+    {
+      icon: <CampaignIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "Digital Marketing",
+      text: "Where innovation meets impact. Your brand, our strategy - a digital synergy like never before!",
+      path: "/services/digital-marketing"
+    },
+    {
+      icon: <ContentPasteIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "Content Marketing",
+      text: "At AiMantra, we provide tailored Social Media Marketing services to elevate your brand's online presence, engagement, and results.",
+      path: "/services/content-marketing"
+    },
+    {
+      icon: <BrushIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+      title: "UI/UX Design",
+      text: "Designing the future with flair! Elevate user experiences and captivate audiences with our expert UI/UX designers.",
+      path: "/services/graphic-designing"
+    },
+];
+
+
+export const processSteps = [
+    {
+      step: "We Access",
+      icon: <SearchIcon sx={{ color: "#fff" }} />,
+      text: "We do end-to-end analysis of a project's scope, scale, deliverables, objectives, and impacts. It also involves assessing the project's stakeholders, risks, timeline, and impacted groups and processes.",
+    },
+    {
+      step: "We Design",
+      icon: <DesignServicesIcon sx={{ color: "#fff" }} />,
+      text: "We design next-generation UI/UX as per the assessment and by considering targeted audience.",
+    },
+    {
+      step: "We Develop",
+      icon: <CodeIcon sx={{ color: "#fff" }} />,
+      text: "We manage the end-to-end deployment of the project and monitor continuously for zero downtime.",
+    },
+    {
+      step: "We Manage",
+      icon: <ManageAccountsIcon sx={{ color: "#fff" }} />,
+      text: "After deploying the project successfully, we manage it completely to provide end-to-end support as per needed.",
+    },
+];
+
+export const visionMissionValues = [
+    {
+      title: "Our Vision",
+      text: `AiMantra's aim is to be known as a global leader in providing innovative, trustworthy and high-quality IT solutions. We aim to provide businesses of all sizes and sectors with cutting-edge technology that optimises operation, enhances the customer experience, and encourages growth.`,
+    },
+    {
+      title: "Our Mission",
+      text: `AiMantra's mission is to redefine industries through the power of artificial intelligence. Committed to innovation and excellence, we strive to provide tailored solutions that empower businesses and enhance user experiences. Guided by a vision of a smarter, more connected world, our mission is to shape the future of AI technology.`,
+    },
+    {
+      title: "Our Values",
+      text: `At AiMantra, our core values drive our commitment to excellence. We prioritize innovation, integrity, and client satisfaction, delivering AI solutions with precision and reliability.`,
+    },
+];
+
+export const whatWeDo = [
+    {
+      title: "Innovative IT Solutions",
+      text: "AiMantra is among the top IT companies in India, with the expertise to deliver complex business solutions that drive measurable impact.",
+    },
+    {
+      title: "Complete Digital Transformation",
+      text: "We provide end-to-end digital solutions tailored to your brand, helping you streamline operations, enhance efficiency, and accelerate growth.",
+    },
+    {
+      title: "AI-Powered Excellence",
+      text: "Leveraging cutting-edge artificial intelligence, AiMantra crafts intelligent services — from smart automation to predictive analytics — shaping a smarter, connected future.",
+    },
+];
+
+export const whyChooseUs = [
+    {
+      title: "Attention to Details",
+      text: "We redefine precision with our unparalleled attention to detail. From coding to deployment, every aspect of our AI solutions is crafted meticulously.",
+      icon: <CheckCircleIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+    },
+    {
+      title: "Highly Qualified Team",
+      text: "Our success is driven by a highly qualified team of experts. With deep AI knowledge, our professionals bring innovation and experience to every project.",
+      icon: <GroupsIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+    },
+    {
+      title: "The Culture",
+      text: "Our culture thrives on innovation, collaboration, and a passion for AI excellence. We foster a dynamic environment committed to learning and client success.",
+      icon: <LightbulbIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+    },
+    {
+      title: "Trusted by 100+ Clients",
+      text: "AiMantra’s proven track record showcases unparalleled expertise, reliability, and transformative solutions — trusted by over 100 satisfied clients.",
+      icon: <HandshakeIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+    },
+    {
+      title: "Long-term Relations",
+      text: "We believe in cultivating lasting partnerships built on trust, adaptability, and shared growth in the evolving landscape of AI.",
+      icon: <FavoriteIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+    },
+    {
+      title: "Excellent Quality & Delivery Time",
+      text: "Excellence is our standard. We ensure every solution meets the highest quality benchmarks and is delivered on time, every time.",
+      icon: <AccessTimeIcon sx={{ fontSize: 40, color: "#0a2540" }} />,
+    },
+];
+
+export const pillars = [
+  "Strategic Assessment: Unveiling Possibilities",
+  "Innovative Design: Crafting User-Centric Solutions",
+  "Agile Development: Bringing Concepts to Life",
+  "End-to-End Management: Ensuring Long-Term Success",
+];
+
+export const aboutOurWork = [
+    {
+      title: "Who We Are",
+      text: "We're a team of creative minds that work hard to achieve our client's goals. Since 2019, we've been delivering innovative technical solutions to businesses of all sizes, helping streamline operations and boost profitability.",
+    },
+    {
+      title: "Revolutionise Your Digital Journey",
+      text: "With AiMantra, innovation meets intelligence. We specialise in cutting-edge AI solutions that elevate your business to unprecedented heights — from smart automation to predictive analytics.",
+    },
+    {
+      title: "Experience the Future of Technology",
+      text: "Our expert team delivers precision, scalability, and transformative results. Unlock the potential of AI-driven excellence with solutions crafted for success.",
+    },
+];
+
+export const features = [
+    {
+      icon: <PersonIcon sx={{ fontSize: 40, color: "primary.main" }} />,
+      title: "Employee Self-Service Portal",
+      desc: "Empower your workforce with access to essential HR functions, allowing them to update personal information, view pay stubs, request time off, and more."
+    },
+    {
+      icon: <WorkIcon sx={{ fontSize: 40, color: "primary.main" }} />,
+      title: "Recruitment and Applicant Tracking",
+      desc: "Streamline your hiring process with automated job postings, resume parsing, and applicant tracking. Ensure you find the best talent quickly and efficiently."
+    },
+    {
+      icon: <AssessmentIcon sx={{ fontSize: 40, color: "primary.main" }} />,
+      title: "Performance Management",
+      desc: "Set goals, track progress, and conduct performance evaluations effortlessly, fostering a culture of continuous improvement within your organization."
+    },
+    {
+      icon: <AccessTimeIcon sx={{ fontSize: 40, color: "primary.main" }} />,
+      title: "Time and Attendance Tracking",
+      desc: "Simplify timekeeping and attendance management with automated tracking, customizable timesheets, and real-time reporting capabilities."
+    },
+    {
+      icon: <PaymentIcon sx={{ fontSize: 40, color: "primary.main" }} />,
+      title: "Payroll Processing",
+      desc: "Say goodbye to manual payroll calculations and compliance headaches. AiMantra HRMS automates payroll processing, tax deductions, and statutory compliance, ensuring accuracy."
+    },
+    {
+      icon: <BarChartIcon sx={{ fontSize: 40, color: "primary.main" }} />,
+      title: "Analytics and Reporting",
+      desc: "Gain valuable insights into your workforce with comprehensive analytics and reporting tools, enabling data-driven decision-making for strategic HR planning."
+    },
+];
+
+export const jobs = [
+    { title: "UI UX Designer", id: "71f59", department: "Information Technology", location: "Gurugram, Haryana", time: "Full-time", experience: "0-5" },
+    { title: "Frontend Developer", id: "79bf9", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-5" },
+    { title: "Mobile Developer", id: "a1211", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-5" },
+    { title: "AI/ML", id: "b2caO", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-5" },
+    { title: "React Native Developer", id: "Oc635", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-2" },
+    { title: "Intern", id: "1a5c1", department: "Information Technology", location: "Gurugram, Haryana", time: "Intern", experience: "Fresher" },
+    { title: "Backend Developer", id: "90870", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-1" },
+    { title: "Full Stack Developer", id: "95744", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-5" },
+    { title: "Video Editor", id: "05417", department: "Information Technology", location: "Gurgaon", time: "Full-time", experience: "0-5" },
+    { title: "Data Analyst Cum Marketing Executive", id: "2170f", department: "Information Technology", location: "Gurgaon", time: "Full-time", experience: "0-5" },
+    { title: "Graphic Designer", id: "72ff0", department: "Information Technology", location: "Gurgaon", time: "Full-time", experience: "0-5" },
+    { title: "Quality Analyst", id: "81996", department: "Information Technology", location: "Gurugram", time: "Full-time", experience: "0-5 Years" },
+];
+
+export const faqs = [
+  {
+    question: "How can I get in touch with AiMantra?",
+    answer:
+      "Connecting with us is a breeze! Simply visit our Contact Us page, fill in the required details, and drop us a message. Alternatively, you can reach out via phone or email, details of which are conveniently listed on our page."
+  },
+  {
+    question: "What information should I include in my message?",
+    answer:
+      "Connecting with us is a breeze! Simply visit our Contact Us page, fill in the required details, and drop us a message. Alternatively, you can reach out via phone or email, details of which are conveniently listed on our page."
+  },
+  {
+    question: "What is the usual response time?",
+    answer: "We understand the importance of timely communication. Rest assured, our team is committed to responding to your queries within 24 hours. Your messages are valuable to us, and we strive to provide prompt and meaningful responses."
+  },
+  {
+    question: "Can I connect with AiMantra through social media?",
+    answer: "Absolutely! Stay updated and engage with us on our social media platforms. Follow us on [LinkedIn](https://www.linkedin.com/company/aimantra) and [Instagram](https://www.linkedin.com/company/aimantra) for the latest updates, industry insights, and a glimpse into the vibrant AiMantra community"
+  },
+  {
+    question: "Is AiMantra hiring? How can I apply for a position?",
+    answer: "Yes, we're always on the lookout for talented individuals to join our team. Visit our Career page for the latest job openings and details on how to apply. We look forward to welcoming new additions to the AiMantra family."
+  },
+  {
+    question: "Can I visit AiMantra's office in person?",
+    answer: "Certainly! Our office is located at Dwarka Expressway, DLF Corporate Greens, 3rd Floor Tower 3, Sec 74A, Gurugram, Haryana 122004, IN. If you like to schedule a visit, drop us a line in advance, and we'll be happy to welcome you."
+  },
+  {
+    question: "How can I get in touch with AiMantra?",
+    answer: "Connecting with us is a breeze! Simply visit our Contact Us page, fill in the required details, and drop us a message. Alternatively, you can reach out via phone or email, details of which are conveniently listed on our page."
+  }
+];
