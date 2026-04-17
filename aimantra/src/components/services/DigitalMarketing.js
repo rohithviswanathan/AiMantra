a041cb8d433e1b41a0c29fcc29c41f74901d4ce9
rohithviswanathan <rohithviswanathan3009@gmail.com>
2026-04-17 17:44:47 +0530

    changed mt and color

--- aimantra/src/components/services/DigitalMarketing.js
+++ aimantra/src/components/services/DigitalMarketing.js
@@ -33,7 +33,7 @@ function DigitalMarketing() {
   ];
 
   return (
-    <Container maxWidth="lg" sx={{ mt: 10, mb: 12 }}>
+    <Container maxWidth="lg" sx={{ mt: 9, mb: 12 }}>
       {/* Headline */}
       <motion.div
         initial={{ opacity: 0, y: -30 }}
@@ -57,7 +57,7 @@ function DigitalMarketing() {
             variant="h4"
             sx={{
               fontWeight: 600,
-              color: "text.secondary",
+              color: "#000000",
               textAlign: "center",
               pb: 2,
             }}
@@ -122,7 +122,7 @@ function DigitalMarketing() {
             variant="h4"
             sx={{
               fontWeight: 600,
-              color: "text.secondary",
+              color: "#000000",
               textAlign: "center",
               mb: 3,
             }}
