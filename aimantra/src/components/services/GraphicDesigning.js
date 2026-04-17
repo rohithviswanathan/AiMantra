4cf508670d37d24947a704d22b9d741c64b233aa
rohithviswanathan <rohithviswanathan3009@gmail.com>
2026-04-17 17:45:11 +0530

    changed mt and color

--- aimantra/src/components/services/GraphicDesigning.js
+++ aimantra/src/components/services/GraphicDesigning.js
@@ -37,7 +37,7 @@ function GraphicDesigning() {
   ];
 
   return (
-    <Container maxWidth="lg" sx={{ mt: 10, mb: 12 }}>
+    <Container maxWidth="lg" sx={{ mt: 9, mb: 12 }}>
       {/* Headline */}
       <motion.div
         initial={{ opacity: 0, y: -30 }}
@@ -61,7 +61,7 @@ function GraphicDesigning() {
             variant="h4"
             sx={{
               fontWeight: 600,
-              color: "text.secondary",
+              color: "#000000",
               textAlign: "center",
               pb: 2,
             }}
@@ -125,7 +125,7 @@ function GraphicDesigning() {
             variant="h4"
             sx={{
               fontWeight: 600,
-              color: "text.secondary",
+              color: "#000000",
               textAlign: "center",
               mb: 3,
             }}
